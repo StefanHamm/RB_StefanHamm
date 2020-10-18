3df1e5ff95e9918651ef22437aee11f4bddcdb08
StefanHamm <stefan.j.hamm@gmail.com>
2020-10-18 15:51:30 +0200

    Initial commit

---
+++ Uebung2_2/Uebung2_2/main.cpp
@@ -0,0 +1,56 @@
+#include "nnxt.h"
+void Timer(){
+    NNXT_LCD_DisplayStringAtLine(0,"Bei Go Druecken bis Stop");
+    Delay(1000);
+    NNXT_LCD_DisplayStringAtLine(0,"                        ");
+    Delay(50);
+    NNXT_LCD_DisplayStringAtLine(0,"3");
+    Delay(1000);
+    NNXT_LCD_DisplayStringAtLine(0,"2");
+    Delay(1000);
+    NNXT_LCD_DisplayStringAtLine(0,"1");
+    Delay(1000);
+    NNXT_LCD_DisplayStringAtLine(0,"GO   ");
+    Delay(50);
+}
+void init(){
+ for(int i=0;i<6;i++)   {
+ NNXT_LCD_DisplayStringAtLine(i,"                                ");
+ Delay(20);
+ }
+}
+int main(){
+init();
+SensorConfig(Port_0, SensorTouch);
+sensor_touch_clicked_t touch;
+SensorConfig(Port_0, SensorTouch);
+float Timespressed=0;
+char Ergebnis[20],clicks[20];
+sensor_touch_clicked_t statebefore=SensorTouch_released;
+
+while(true){
+  Timer();
+    uint32_t starttime=GetSysTime();
+    uint32_t endtime=GetSysTime();
+    while((starttime+10000)>=endtime){
+        Touch_Clicked(Port_0, &touch);
+        if((touch==SensorTouch_clicked)&&(statebefore==SensorTouch_released)){
+            Timespressed++;
+            statebefore=SensorTouch_clicked;
+            }
+        statebefore=touch;
+        endtime=GetSysTime();
+            Delay(10);
+    }
+    NNXT_LCD_DisplayStringAtLine(0,"Stop   ");
+    Delay(1000);
+    NNXT_LCD_DisplayStringAtLine(2,"Ergebnis");
+    Timespressed=Timespressed/10;
+    sprintf(Ergebnis,"%.2f HZ",Timespressed);
+    NNXT_LCD_DisplayStringAtLine(3,Ergebnis);
+    Delay (3000);
+    Timespressed=0;
+
+}
+    return 0;
+}
---
+++ Uebung2_4/Uebung2_4/main.cpp
@@ -0,0 +1,80 @@
+#include "nnxt.h"
+volatile uint8_t Rclick=0;
+volatile uint8_t Lclick=0;
+void rechtsstask(){
+    sensor_touch_clicked_t touch =SensorTouch_released,statebefore=SensorTouch_released;
+
+while(true){
+
+        Touch_Clicked(Port_0, &touch);
+
+        if((touch==SensorTouch_clicked)&&(statebefore==SensorTouch_released)){
+       
+              Rclick=1;
+            }
+
+        statebefore=touch;
+
+        Delay(100);
+}
+
+    }
+
+void linksstask(){
+    sensor_touch_clicked_t touch =SensorTouch_released,statebefore=SensorTouch_released;
+
+while(true){
+
+        Touch_Clicked(Port_1, &touch);
+        if((touch==SensorTouch_clicked)&&(statebefore==SensorTouch_released)){
+            Lclick=1;
+        }
+        statebefore=touch;
+
+        Delay(100);
+
+    }
+
+
+}
+void Task3(){
+    motor_dir_t Richtung=Motor_dir_forward;
+
+
+while(1){
+      if(Rclick){
+        switch(Richtung){
+    case Motor_dir_forward:Richtung=Motor_dir_backward;NNXT_LCD_DisplayStringAtLine(0,"Rückwärts ");break;
+    case Motor_dir_backward:Richtung=Motor_dir_forward;NNXT_LCD_DisplayStringAtLine(0,"Vorwärts ");break;
+    default:break;
+        }
+        Rclick=0;
+      }
+
+    if(Lclick==1){
+
+        Motor_Drive(Port_A,Richtung,35);
+        Motor_Drive(Port_B,Richtung,35);
+        Delay(1000);
+        Motor_Stop(Port_A,Motor_stop_break);
+        Motor_Stop(Port_B,Motor_stop_break);
+        Lclick=0;
+    }
+
+
+}
+}
+int main(){
+    NNXT_LCD_DisplayStringAtLine(0,"Vorwärts ");
+    SensorConfig(Port_0,SensorTouch);
+    SensorConfig(Port_1,SensorTouch);
+    MotorPortInit(Port_A);
+    MotorPortInit(Port_B);
+CreateAndStartTask(rechtsstask);
+CreateAndStartTask(linksstask);
+CreateAndStartTask(Task3);
+
+StartScheduler();
+
+    return 0;
+}
